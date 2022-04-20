e7716976bbaf61bd2a96e89b5a0ad5738c792a80
Javier Zaleta <94091554+J-cordz@users.noreply.github.com>
2022-04-20 17:36:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-Hello World
+tool.fixedDistance = 30
+
+function onMouseMove(event) {
+  var path = new Path.Circle({
+    center: event.middlePoint,
+    radius: Math.round(Math.random() * 30) + 5
+  })
+  path.fillColor = {
+    hue: event.count * 3,
+    saturation: 1,
+    brightness: 1
+  }
+}
