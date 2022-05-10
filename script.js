81f5d45edbaaa2382bf90edac47927b58f818184
Javier Zaleta <94091554+J-cordz@users.noreply.github.com>
2022-05-10 17:28:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,36 @@ function onMouseMove(event) {
   }
 }
 
-var myPath;
+tool.minDistance = 10;
+tool.maxDistance = 45;
 
-        function onMouseDown(event) {
-            myPath = new Path();
-            myPath.strokeColor = 'teal';
-            myPath.strokeWidth =8;
-        }
-        
-        function onMouseDrag(event) {
-            myPath.add(event.point);
-        }
+var path;
+
+function onMouseDown(event) {
+	path = new Path();
+	path.fillColor = {
+		hue: Math.random() * 360,
+		saturation: 1,
+		brightness: 1
+	};
+
+	path.add(event.point);
+}
+
+function onMouseDrag(event) {
+	var step = event.delta / 2;
+	step.angle += 90;
+	
+	var top = event.middlePoint + step;
+	var bottom = event.middlePoint - step;
+	
+	path.add(top);
+	path.insert(0, bottom);
+	path.smooth();
+}
+
+function onMouseUp(event) {
+	path.add(event.point);
+	path.closed = true;
+	path.smooth();
+}
