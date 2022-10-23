9b3df09d61d7dc2be3401315c97ab546cc3c047e
Javier Zaleta Martínez <94091554+javierzaleta@users.noreply.github.com>
2022-10-23 12:39:25 -0500

    Update script formatting

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-tool.fixedDistance = 30
-var layer = project.activeLayer
+tool.fixedDistance = 30;
+var layer = project.activeLayer;
 
 function onMouseMove(event) {
   var path = new Path.Circle({
     center: event.middlePoint,
-    radius: Math.round(Math.random() * 30) + 5
-  })
+    radius: Math.round(Math.random() * 30) + 5,
+  });
   path.fillColor = {
     hue: event.count * 3,
     saturation: 1,
-    brightness: 1
-  }
+    brightness: 1,
+  };
 }
 
 tool.minDistance = 10;
@@ -19,30 +19,30 @@ tool.maxDistance = 45;
 var path;
 
 function onMouseDown(event) {
-	path = new Path();
-	path.fillColor = {
-		hue: Math.random() * 360,
-		saturation: 1,
-		brightness: 1
-	};
+  path = new Path();
+  path.fillColor = {
+    hue: Math.random() * 360,
+    saturation: 1,
+    brightness: 1,
+  };
 
-	path.add(event.point);
+  path.add(event.point);
 }
 
 function onMouseDrag(event) {
-	var step = event.delta / 2;
-	step.angle += 90;
-	
-	var top = event.middlePoint + step;
-	var bottom = event.middlePoint - step;
-	
-	path.add(top);
-	path.insert(0, bottom);
-	path.smooth();
+  var step = event.delta / 2;
+  step.angle += 90;
+
+  var top = event.middlePoint + step;
+  var bottom = event.middlePoint - step;
+
+  path.add(top);
+  path.insert(0, bottom);
+  path.smooth();
 }
 
 function onMouseUp(event) {
-	path.add(event.point);
-	path.closed = true;
-	path.smooth();
+  path.add(event.point);
+  path.closed = true;
+  path.smooth();
 }
